9489c0bbe2530e026c185adf6ec9d813fd8d80b3
Weldisson Araujo <weldisson.a.silva@accenture.com>
2024-08-01 18:10:58 -0300

    test: make tests to user

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,178 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Address } from './entities/address.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { Repository } from 'typeorm';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
+  let userRepository: Repository<User>;
+  let addressRepository: Repository<Address>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        { provide: getRepositoryToken(User), useClass: Repository },
+        { provide: getRepositoryToken(Address), useClass: Repository },
+      ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get<UserService>(UserService);
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
+    addressRepository = module.get<Repository<Address>>(
+      getRepositoryToken(Address),
+    );
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a user successfully', async () => {
+      const createUserDto = {
+        name: 'John',
+        lastname: 'Doe',
+        cpf: '123.456.789-00',
+        birthDate: '1990-01-01',
+        addresses: [
+          {
+            street: '123 Main St',
+            additional: 'Apt 4B',
+            city: 'New York',
+            state: 'NY',
+            zip: '10001',
+          },
+        ],
+      } as unknown as CreateUserDto;
+      const userEntity = { ...createUserDto, id: 1 };
+
+      jest.spyOn(userRepository, 'create').mockReturnValue(userEntity as any);
+      jest
+        .spyOn(addressRepository, 'create')
+        .mockReturnValue(createUserDto.addresses[0] as any);
+      jest.spyOn(userRepository, 'save').mockResolvedValue(userEntity as any);
+
+      await userService.create(createUserDto);
+
+      expect(userRepository.create).toHaveBeenCalledWith(createUserDto);
+      expect(userRepository.save).toHaveBeenCalledWith(userEntity);
+    });
+
+    it('should throw an error if creation fails', async () => {
+      const createUserDto = {
+        name: 'John Doe',
+        cpf: '12345678901',
+      } as unknown as CreateUserDto;
+
+      jest.spyOn(userRepository, 'create').mockImplementation(() => {
+        throw new Error('Test error');
+      });
+
+      await expect(userService.create(createUserDto)).rejects.toThrow(Error);
+    });
+  });
+
+  describe('findByName', () => {
+    it('should find users by name successfully', async () => {
+      const users = [
+        { id: 1, name: 'John Doe', cpf: '12345678901', addresses: [] },
+      ];
+
+      jest.spyOn(userRepository, 'find').mockResolvedValue(users as any);
+
+      const result = await userService.findByName('John Doe');
+
+      expect(userRepository.find).toHaveBeenCalledWith({
+        where: { name: 'John Doe' },
+        relations: ['addresses'],
+      });
+      expect(result).toEqual(users);
+    });
+
+    it('should throw NotFoundException if no users found', async () => {
+      jest.spyOn(userRepository, 'find').mockResolvedValue([]);
+
+      await expect(userService.findByName('John Doe')).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe('findByCpf', () => {
+    it('should find user by CPF successfully', async () => {
+      const user = { id: 1, name: 'John Doe', cpf: '12345678901' };
+
+      jest.spyOn(userRepository, 'findOne').mockResolvedValue(user as any);
+
+      const result = await userService.findByCpf('12345678901');
+
+      expect(userRepository.findOne).toHaveBeenCalledWith({
+        where: { cpf: '12345678901' },
+      });
+      expect(result).toEqual(user);
+    });
+
+    it('should throw NotFoundException if user not found', async () => {
+      jest.spyOn(userRepository, 'findOne').mockResolvedValue(null);
+
+      await expect(userService.findByCpf('12345678901')).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe('update', () => {
+    it('should update user successfully', async () => {
+      const updateUserDto: UpdateUserDto = { name: 'John Doe Updated' };
+      const userEntity = { id: 1, ...updateUserDto };
+
+      jest
+        .spyOn(userRepository, 'preload')
+        .mockResolvedValue(userEntity as any);
+      jest.spyOn(userRepository, 'save').mockResolvedValue(userEntity as any);
+
+      const result = await userService.update(1, updateUserDto);
+
+      expect(userRepository.preload).toHaveBeenCalledWith({
+        id: 1,
+        ...updateUserDto,
+      });
+      expect(userRepository.save).toHaveBeenCalledWith(userEntity);
+      expect(result).toEqual(userEntity);
+    });
+
+    it('should throw NotFoundException if user not found for update', async () => {
+      jest.spyOn(userRepository, 'preload').mockResolvedValue(null);
+
+      await expect(
+        userService.update(1, { name: 'John Doe Updated' }),
+      ).rejects.toThrow(NotFoundException);
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove user successfully', async () => {
+      jest
+        .spyOn(userRepository, 'delete')
+        .mockResolvedValue({ affected: 1 } as any);
+
+      await userService.remove(1);
+
+      expect(userRepository.delete).toHaveBeenCalledWith(1);
+    });
+
+    it('should throw NotFoundException if user not found for removal', async () => {
+      jest
+        .spyOn(userRepository, 'delete')
+        .mockResolvedValue({ affected: 0 } as any);
+
+      await expect(userService.remove(1)).rejects.toThrow(NotFoundException);
+    });
   });
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,97 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { faker } from '@faker-js/faker';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { CreateUserDto } from './../src/user/dto/create-user.dto';
+import { UpdateUserDto } from './../src/user/dto/update-user.dto';
 
-describe('AppController (e2e)', () => {
+describe('UserController (e2e)', () => {
   let app: INestApplication;
-
-  beforeEach(async () => {
+  let userId: number;
+  const name: string = faker.string.fromCharacters('test', 5);
+  const cpf: string = faker.string.numeric(11);
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+    );
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/user (POST) - should create a user', async () => {
+    const createUserDto = {
+      name: name,
+      lastname: 'Doe',
+      cpf: cpf,
+      birthDate: '1990-01-01',
+      addresses: [
+        {
+          street: '123 Main St',
+          additional: 'Apt 4B',
+          city: 'New York',
+          state: 'NY',
+          zip: '10001',
+        },
+      ],
+    } as unknown as CreateUserDto;
+
     return request(app.getHttpServer())
-      .get('/')
+      .post('/user')
+      .send(createUserDto)
+      .expect(201);
+  });
+
+  it(`/user?name=${name} (GET) - should find users by name`, async () => {
+    return request(app.getHttpServer())
+      .get('/user')
+      .query({ name: name })
       .expect(200)
-      .expect('Hello World!');
+      .expect((res) => {
+        expect(res.body).toHaveLength(1);
+        expect(res.body[0].name).toBe(name);
+        userId = res.body[0].id;
+      });
+  });
+
+  it(`/user?cpf=${cpf} (GET) - should find user by CPF`, async () => {
+    return request(app.getHttpServer())
+      .get('/user')
+      .query({ cpf: cpf })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.cpf).toBe(cpf);
+      });
+  });
+
+  it('/user/:id (PATCH) - should update a user', async () => {
+    const updateUserDto: UpdateUserDto = {
+      name: 'Alex',
+    };
+
+    return request(app.getHttpServer())
+      .patch(`/user/${userId}`)
+      .send(updateUserDto)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.name).toBe('Alex');
+      });
+  });
+
+  it('/user/:id (DELETE) - should delete a user', async () => {
+    return request(app.getHttpServer())
+      .delete(`/user/${userId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.message).toBe('User removed successfully');
+      });
   });
 });
